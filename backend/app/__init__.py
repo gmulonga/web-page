1ee43454d311f19746bf65124efa04a4d8b78eca
gmulonga <gadsonmulonga@gmail.com>
2024-06-02 17:45:35 +0300

    perf: changing the structure of init.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -10,23 +10,24 @@ ma = Marshmallow()
 jwt = JWTManager()
 
 
-def create_app():
-    """creates a flask app"""
-    app = Flask(__name__, instance_relative_config=True)
+def create_app() -> Flask:
+    """Creates a Flask app"""
+    app = Flask(__name__)
     app.config.from_object('app.config')
-    app.config.from_pyfile('config.py')
 
     db.init_app(app)
     ma.init_app(app)
     jwt.init_app(app)
-    CORS(app, origins="http://localhost:3000")
+    CORS(app, origins="*")
 
+    # Register blueprints
+    from . import cars_routes, requests_routes, admin_routes
+    app.register_blueprint(admin_routes.main)
+    app.register_blueprint(cars_routes.cars_bp)
+    app.register_blueprint(requests_routes.request_bp)
+
+    # Create database tables
     with app.app_context():
-        from . import cars_routes, requests_routes, admin_routes
-        app.register_blueprint(admin_routes.main)
-        app.register_blueprint(cars_routes.cars_bp)
-        app.register_blueprint(requests_routes.request_bp)
-
         db.create_all()
 
     return app
