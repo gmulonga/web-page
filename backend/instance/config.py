08a9d60cd4796ccfa38853336dbb53cd4ab4bb8c
gmulonga <gadsonmulonga@gmail.com>
2024-06-03 15:32:47 +0300

    perf: refactoring the config file

--- backend/instance/config.py
+++ backend/instance/config.py
@@ -1,7 +1,8 @@
 import os
+import secrets
 
 
-SECRET_KEY = os.urandom(24)
-JWT_SECRET_KEY = os.urandom(24)
+SECRET_KEY = secrets.token_bytes(24)
+JWT_SECRET_KEY = secrets.token_bytes(24)
 SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
